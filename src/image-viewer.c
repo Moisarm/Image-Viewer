28a2dffdcaff75c103bb68a950e8240e1a429d47
Moisarm <moisarm03@gmail.com>
2026-04-08 22:53:33 -0400

    [feature] (main) : Now the program is processing an image, that is centered in the screen

--- src/image-viewer.c
+++ src/image-viewer.c
@@ -8,6 +8,18 @@
 #include "../include/runara/include/runara/runara.h"
 
 #define BACKGROUND_COLOR 0.3f, 0.1f, 0.4f, 1.0f
+#define MAX(a, b) a > b ? a : b
+#define MIN(a, b) a < b ? a : b
+
+typedef struct {
+    RnState* render;
+
+} global_state;
+
+static global_state s;
+void resize_callback(GLFWwindow* window, int width, int height) {
+    rn_resize_display(s.render, width, height);
+}
 
 int main() {
     const unsigned int SCREEN_WIDTH = 800;
@@ -33,12 +45,16 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    RnState* state = rn_init(SCREEN_WIDTH, SCREEN_HEIGHT, (RnGLLoader)glfwGetProcAddress);
+    glfwSetFramebufferSizeCallback(window, resize_callback);
 
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         fprintf(stderr, "Error initializing GLAD\n");
         return -1;
     }
+    s.render = rn_init(SCREEN_WIDTH, SCREEN_HEIGHT, (RnGLLoader)glfwGetProcAddress);
+
+    RnTexture image = rn_load_texture("./gojo.jpg");
 
     while (!glfwWindowShouldClose(window)) {
         //This function set the color of the screen but this doesn't paint the screen
@@ -46,16 +62,32 @@ int main() {
         //This is the function that paints the screen with the color set by the glClearColor 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        rn_begin(state);
-        rn_rect_render(state, (vec2s){50.0f, 50.0f}, 
-                              (vec2s){50.0f, 50.0f}, RN_RED);
-        rn_end(state);
+        //rn_clear_color(rn_color_from_hex(0x191970ff));
+
+        rn_begin(s.render);
+        
+        float best_image_ratio = MIN
+        (
+            s.render->render_w / (float)image.width, 
+            s.render->render_h / (float)image.height
+        );
+
+        vec2s image_size = (vec2s){image.width * best_image_ratio, image.height * best_image_ratio};
+        vec2s image_position = (vec2s){(s.render->render_w - image_size.x) /2.0f, (s.render->render_h - image_size.y) /2.0f};
+        
+        
+        rn_image_render(s.render, image_position, RN_WHITE, (RnTexture){
+            .width = (uint32_t)image_size.x,
+            .height = (uint32_t)image_size.y,
+            .id = image.id
+        });
+        rn_end(s.render);
 
         glfwSwapBuffers(window);
         glfwWaitEvents();  
     }
 
-    rn_terminate(state);  // Clean
+    rn_terminate(s.render);  // Clean
     glfwDestroyWindow(window);
     glfwTerminate();
     printf("Window Closed");
