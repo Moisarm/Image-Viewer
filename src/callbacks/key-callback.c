ef6464e6e1d788c9cfe1e36ab5828d7f674a4082
Moisarm <moisarm03@gmail.com>
2026-05-31 23:46:24 -0400

    [feature] (experimental) : added the f11 full screen functionality in the key callback

--- src/callbacks/key-callback.c
+++ src/callbacks/key-callback.c
@@ -17,6 +17,24 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         else if (key == GLFW_KEY_ESCAPE) {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
         }
+        else if (key == GLFW_KEY_F11){
+            if(state->is_full_screen){
+                //Save current position and size of the screen
+                glfwGetWindowPos(window, &state->window_x, &state->window_y);
+                glfwGetWindowSize(window, &state->window_width, &state->window_height);
+
+
+                //Get the principal monitor and and its video mode
+                GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+                const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+                //Activate the full screen
+                glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+            }else{
+                //Deactivate the full screen and set the window to it's previous position and size
+                glfwSetWindowMonitor(window, NULL, state->window_x, state->window_y, state->window_width, state->window_height, 0);
+            }
+        }
 
         // if path change, should reload the texture
         if (next_path) {
